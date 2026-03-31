ce1ba9ce6ec9a130e0b2324e116083d047fa117b
ccqsantos <caua0.cicarone@gmail.com>
2026-03-31 11:55:01 -0300

    initialized react with vite

---
+++ GereciamentoAlunosFRONTEND/GerenciamentoDeAlunos/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+  return (
+        <div>
+        <h1>To Do List Application</h1>
+        <input type="text" id="txt-task"/>
+            <button id="btn-task">Adicionar</button>
+            <div id="list-task"></div>
+        </div>
+  )
+}
+
+export default App;
