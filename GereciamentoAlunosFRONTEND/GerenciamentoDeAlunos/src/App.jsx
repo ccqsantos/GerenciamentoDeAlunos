d4eb0577a2d5115de25d5681088137594d616e67
Henrique Tashiro <henriquetashiro17@gmail.com>
2026-04-01 14:04:19 -0300

    pequena adicao no front

--- GereciamentoAlunosFRONTEND/GerenciamentoDeAlunos/src/App.jsx
+++ GereciamentoAlunosFRONTEND/GerenciamentoDeAlunos/src/App.jsx
@@ -2,11 +2,33 @@ import './App.css'
 
 const App = () => {
   return (
+        <div id="Form">
+        <h1>Cadastro De Alunos</h1>
         <div>
-        <h1>To Do List Application</h1>
+        <form id="cadastro"> 
+          <label>Nome: </label>
         <input type="text" id="txt-task"/>
-            <button id="btn-task">Adicionar</button>
+
+        <label>Data de Nascimento: </label>
+        <input type="date" id="date-task"></input>
+
+        <label for="curso">Selecione seu curso:</label>
+
+         <select id="curso" name="curso" required>
+          <option value="Desenvolvimento de Software">Desenvolvimento de Software</option>
+            <option value="Logistica">Logistica</option>
+            <option value="Gestão Empresarial">Gestão Empresarial</option>
+            <option value="Ciencias Sociais"> Ciencias Sociais</option>
+            <option value="Economia"  >Economia</option>
+          </select>
+
+          <label>Matricula</label>
+          <input type="text" id="mat-task"></input>
+
+            <button id="btn-task">Cadastrar</button>
             <div id="list-task"></div>
+            </form>
+            </div>
         </div>
   )
 }
